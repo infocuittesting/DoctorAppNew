f2817445a683c98e6715a9e43dbac4eddfa24c61
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-02-20 14:21:04 +0530

    add new code for sms

--- SendSMS.py
+++ SendSMS.py
@@ -54,3 +54,17 @@ def sendsms(request):
        # return (json.dumps({"Message": "Sms Sent UnSuccessfully", "Message_Code": "SSUS", "Service_Status": "Failure"},
                            #indent=2))
 
+def Sendmessge(request):
+         mobile = request.json['mobile']
+         message = request.json['message']
+         code =  request.json['code']
+         print(mobile, message, code)
+         url = "https://control.msg91.com/api/sendhttp.php?authkey=195833ANU0xiap5a708d1f&mobiles="+mobile+"&message="+message+"&sender=Infoit&route=4&country="+code+""
+         req = urllib.request.Request(url)
+         with urllib.request.urlopen(req) as response:
+            the_page = response.read()
+            the_page = the_page[1:]
+            print(the_page)
+            the_page = str(the_page)
+         return(json.dumps({"Message":"SMS Sent Sucessfully","Key":the_page},indent =2))
+        #return("hi")    
