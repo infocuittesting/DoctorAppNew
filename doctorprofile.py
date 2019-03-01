20b70a297a319060f1a51cecd46496b5c6fe84b5
infocuittesting <38007487+infocuittesting@users.noreply.github.com>
2019-03-01 18:51:21 +0530

    change update_doctor_profile

--- doctorprofile.py
+++ doctorprofile.py
@@ -66,35 +66,70 @@ def update_Businessprofile(request):
 def update_doctorprofile(request):
     try:
         doctors = request.json
+        w_time={}
+        w_time['average_waiting_time']=doctors['average_waiting_time']
+        
         speclaization = doctors['Specialization']
         service = doctors['services']
-        doc_record = {k : v for k,v in doctors.items() if k not in ('Specialization','services')}
-        doctor = {k : v for k,v in doctors.items() if k  in ('doctor_id')}
-        doctorid = json.loads(dbget("select count(*) as doctor_id from new.doctor_profile where doctor_profile_id ='"+d['doctor_id']+"'"))
-        businessid = json.loads(dbget("select count(*) as business_id from new.business_profile where business_id ='"+str(d['business_id'])+"'"))
-        if doctorid[0]['doctor_id'] == 1 and businessid[0]['business_id'] == 1 and  len(speclaization) != 0 and len(service) != 0:
-            doc = doctor.get('doctor_id')
-            doc_record['doctor_profile_id'] = doctors['doctor_name'][:4]+str(doctor.get('doctor_id'))
+        day = doctors['days']
+        doctor_id=doctors['doctor_id']
+        doc_record = {k : v for k,v in doctors.items() if k not in ('Specialization','services','days','business_id','average_waiting_time','doctor_id')}
+        print(doc_record )
+        doctor = {k : v for k,v in doctors.items() if k  in ('doctor_profile_id')}
+        print('mohan',doctor)
+        doctorid = json.loads(dbget("select count(*) as doctor_id from new.doctor_profile where doctor_profile_id  ='"+(doctors['doctor_profile_id'])+"'"))
+        print(doctorid)
+        #businessid = json.loads(dbget("select count(*) as business_id from new.business_profile where business_id ='"+str(doctors['business_id'])+"'"))
+        #if doctorid[0]['doctor_id'] == 1 and businessid[0]['business_id'] == 1 and  len(speclaization) != 0 and len(service) != 0:
+        if 1==1:
+            doc = doctor.get('doctor_profile_id')
+            print(doc)
+            #doc_record['doctor_profile_id'] = doctors['doctor_name'][:4]+str(doctor.get('doctor_profile_id'))
+            print(doc_record)
             gensql('update','new.doctor_profile',doc_record,doctor)
             doc_profile_id = doc_record['doctor_profile_id']
+            print(doc_profile_id)
             d = {'doctor_id':doc_profile_id}
-            dbput("delete from new.doctor_specialization where doctor_id='"+str(d)+"'")
+           
+            dbput("delete from new.doctor_specialization where doctor_id='"+str(doc_profile_id)+"'")
             for sep in speclaization:
                 i = {}
                 i['specialization_id'] = sep
                 i['doctor_id'] = doc_profile_id
                 gensql('insert','new.doctor_specialization',i)
-            dbput("delete from new.doctor_services where doctor_id='"+str(d)+"'")    
+            dbput("delete from new.doctor_services where doctor_id='"+str(doc_profile_id)+"'")    
             for ser in service:
                 i = {}
                 i['doctor_id'] = doc_profile_id
                 i['service_id'] = ser
+                print('qwertyu',i)
                 gensql('insert','new.doctor_services',i)
+           
+            #businessid = json.loads(dbget("select count(*) as bus_id from new.timing where business_id='"+(doctors['business_id'])+"'"))
+            dbput("delete from new.timing where doctor_id='"+doc_profile_id+"' and business_id='"+doctors['business_id']+"' ")
+            for tim in day:
+                print(day)
+                tim['doctor_id'] = doc_profile_id
+                tim['business_id'] = doctors['business_id']
+                gensql('insert','new.timing',tim)
+    
+                #print(wait_time)
+            avg={}
+            print(avg)
+            print(doctor_id)
+            avg['doctor_id'] = doctor_id
+            print(avg)
+            avg['business_id'] = doctors['business_id']
+            print('avg', avg)
+            #print(average_waiting_time, type(average_waiting_time))
+            gensql('update','new.doctorinbusiness', w_time ,avg)
+
+            
             return(json.dumps({"Message":"Recored Updated Successfully","Message_Code":"RUS","Service_Status":"Success"},indent=4))
         else:
             return(json.dumps({'Message': 'Invalid Data', 'Message_Code': 'ID', 'Status': 'Failure'},indent=4))
     except:
-        return(json.dumps({"Message":"Record Updated Unsuccessful","Message_Code":"RUUS","Service_Status":"Success"},indent=4))  
+       return(json.dumps({"Message":"Record Updated Unsuccessful","Message_Code":"RUUS","Service_Status":"Success"},indent=4))  
 
     
 def update_businessanddoctors(request):
